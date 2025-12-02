8702f8a5f36c6f28a4a2c4b4b0f7b370837f68bd
Dark_Thunderer <darkthunderer@mail.ru>
2025-12-03 04:06:20 +0500

    Пу-пу-пу, я рак в гите и гитхабе. Немного увлекся с редакцией перевода, надеюсь все мои правки будут к месту. Если же вам показалось что я что-то сделал зря, - не стесняйтесь мне об этом сообщить. Надеюсь понравится.

--- localization/basement.js
+++ localization/basement.js
@@ -15,27 +15,27 @@ env.localization.page['fbx'] = {dialogues: {},
         "about this job": "об этой работе",
 
         "DENDRITIC CYST": "ДЕНДРИТНАЯ ЦИСТА",
-        "touch": "потрогать",
+        "touch": "коснуться",
         "lift": "поднять",
-        "the notes I got say that there were apparently a ton of these on the ship": "в записях говорилось, что на корабле вот таких вот была просто куча",
+        "the notes I got say that there were apparently a ton of these on the ship": "в отчете указано, что таких на корабле была целая куча",
         "all sorts of sizes too": "причём самых разных размеров",
-        "but most were too heavy to retrieve... so we just got this small one": "но большинство были слишком тяжёлыми... так что захватили только одну маленькую",
+        "but most were too heavy to retrieve... so we just got this small one": "большинство - слишком тяжелые, чтобы их можно было поднять. Эта - одна из тех, что поменьше.",
         "the dendritic cyst has a rigid outer shell. it's unlikely that there's any way to connect to it": "у дендритной цисты жёсткая внешняя оболочка. не похоже, что к ней можно подключиться",
-        "the dendritic cyst is surprisingly heavy. turning it in your hands produces a cascade of metallic clinking noises from within. when you set it back down, its tendrils find a new orientation to support itself on the table": "дендритная циста неожиданно тяжела. при повороте изнутри раздаётся каскад звенящих металлических звуков. когда ты ставишь её обратно, её отростки находят новый способ удержаться на столе",
+        "the dendritic cyst is surprisingly heavy. turning it in your hands produces a cascade of metallic clinking noises from within. when you set it back down, its tendrils find a new orientation to support itself on the table": "дендритная циста на удивление тяжела. при поворотах слышно как внутри что-то звенит. едва ты опускаешь цисту на стол, ее отростки сдвигаются, чтобы равномерно перераспределить вес по поверхности",
 
         "FRACTALLINE CYST": "ФРАКТАЛЬНАЯ ЦИСТА", 
-        "nobody knows what this one is.. pretty sure it's dead though": "никто не знает, что именно это такое.. но думаю она дохлая",
-        "the fractalline cyst's outer shell is slimy and has some yield. if you squeezed it enough, it would probably turn to sludge... best to just leave this one alone": "оболочка фрактальной цисты склизкая и упругая. если ты сожмёшь её сильнее, она наверняка лопнет... лучше оставить её в покое",
+        "nobody knows what this one is.. pretty sure it's dead though": "из наших никто не знает что это такое. Думаю оно уже сдохло.",
+        "the fractalline cyst's outer shell is slimy and has some yield. if you squeezed it enough, it would probably turn to sludge... best to just leave this one alone": "оболочка фрактальной цисты склизская и упругая. сожмешь чуть сильнее - наверняка лопнет.. лучше не испытывать судьбу",
 
         "CYST": "ЦИСТА", 
-        "so this is the mystery piece": "а вот и гость программы",
-        "it's in bizarrely good condition considering it was at the bottom of the ocean for however long it's been since, y'know": "в поразительно хорошем состоянии, учитывая сколько она провалялась на дне океана с тех пор как, ну знаешь",
-        "no clue what it is aside from some similar internal structure to network cysts we've found before": "без малейшего понятия что она делает, но её внутренности напоминают ранее найденные сетевые цисты",
-        "the cyst has a solid outer shell. a few circular points near the top are less firm than the rest. in your experience, these are usually connection points. you can definitely scan this point more thoroughly.": "у цисты твёрдая внешняя оболочка, однако несколько круглых точек наверху менее плотные, чем всё остальное. в твоём опыте это обычно точки соединения. ты определённо можешь просканировать их более тщательно.",
+        "so this is the mystery piece": "а вот и гвоздь программы",
+        "it's in bizarrely good condition considering it was at the bottom of the ocean for however long it's been since, y'know": "в поразительно хорошем состоянии, учитывая как долго ей довелось проваляться на дне океана",
+        "no clue what it is aside from some similar internal structure to network cysts we've found before": "понятия не имею для чего она нужна - ясно лишь что внутренняя структура походит на таковую у сетевых цист",
+        "the cyst has a solid outer shell. a few circular points near the top are less firm than the rest. in your experience, these are usually connection points. you can definitely scan this point more thoroughly.": "оболочка цисты довольно твердая. в нескольких местах сверху цисты оболочка чуть тоньше. как подсказывает твой опыт, через такие точки легче всего установить соединение. стоит чуть внимательнее их изучить.",
         "depth scan": "подробное сканирование",
         "ANALYSIS::'valid nerve point';'connection enabled'": "АНАЛИЗ::'действительная нервная точка';'доступно подключение'",
         "::CORRUCYSTIC ENTITY": "`::КОРРУЦИСТОЗНАЯ СУЩНОСТЬ",
-        "::NO FUNCTION SIGNATURE": "::НЕТ ПОДПИСИ ФУКНЦИИ",
+        "::NO FUNCTION SIGNATURE": "::БЕЗ ПОДПИСИ ФУНКЦИИ",
         "::CONNECTIVE MEMBRANE EXPOSED": "::ОБНАЖЁННАЯ СОЕДИНИТЕЛЬНАЯ МЕМБРАНА",
 
         "CYSTIC COLUMN": "ЦИСТОЗНАЯ КОЛОННА", 
@@ -54,23 +54,23 @@ env.localization.page['fbx'] = {dialogues: {},
         "kill dendritic cyst and feed column": "убить дендритную цисту и покормить колонну",
 
         "hey buddy": "эй дружище",
-        "welcome back": "добро пожаловать",
+        "welcome back": "рад тебя видеть",
         "you ready to feed this thing?": "ну что, накормим эту штуку?",
         "haven't heard back on the request yet": "со мной ещё не связались по поводу запроса",
-        "so, still killing time here": "так что пока убиваем время"
+        "so, still killing time here": "так что пока делаем что хочется"
     },
     entityDescriptions: {
         "dendritic cyst": `::КОРРУЦИСТОЗНАЯ СУЩНОСТЬ
-::ПОДПИСЬ ФУКНЦИИ: КОНТЕЙНЕР
+::ПОДПИСЬ ФУНКЦИИ: КОНТЕЙНЕР
 ::НЕТ ОБНАЖЁННОЙ СОЕДИНИТЕЛЬНОЙ ТКАНИ`,
         "fractalline cyst": `::КОРРУЦИСТОЗНАЯ СУЩНОСТЬ
-::ПОВРЕЖДЁННАЯ ПОДПИСЬ ФУКНЦИИ
+::ПОВРЕЖДЁННАЯ ПОДПИСЬ ФУНКЦИИ
 ::НЕТ ОБНАЖЁННОЙ СОЕДИНИТЕЛЬНОЙ ТКАНИ`,
         "cystic column": `::КОРРУЦИСТОЗНАЯ СУЩНОСТЬ
-::ПОДПИСЬ ФУКНЦИИ: ОБСЛУЖИВАНИЕ КОРРУЦИСТЫ
+::ПОДПИСЬ ФУНКЦИИ: ОБСЛУЖИВАНИЕ КОРРУЦИСТЫ
 ::НЕТ ОБНАЖЁННОЙ СОЕДИНИТЕЛЬНОЙ ТКАНИ`,
         "cyst": `::КОРРУЦИСТОЗНАЯ СУЩНОСТЬ
-::НЕТ ПОДПИСИ ФУКНЦИИ
+::НЕТ ПОДПИСИ ФУНКЦИИ
 ::ОБНАЖЁННАЯ СОЕДИНИТЕЛЬНАЯ МЕМБРАНА`,
         "connection point": `::КОМПОНЕНТ КОРРУЦИСТОЗНОЙ СУЩНОСТИ
 ::ДЕЙСТВИТЕЛЬНАЯ ТОЧКА СОЕДИНЕНИЯ`,
@@ -142,9 +142,9 @@ env.localization.page['fbx'].dialogues["index"] = generateDialogueObject(`
 start 
     moth
         эй, дружище, добро пожаловать 
-        честно у меня были сомнения, что ты появишься на такой мелкий улов
-        в последнее время находят столько кораблекрушений
-        в любом случае сейчас я всё запущу, можешь пока осмотреться
+        честно говоря, я думал ты не клюнешь на такую мелкую рыбеху
+        в конце концов, слишком уж много сейчас в море находят обломков
+        ладно, пока я все тут подготавливаю - можешь посмотреть с чем мы имеем дело
         
     sourceless
         декодирующие машины включаются, бросая свет на паутину проводов.
@@ -160,13 +160,13 @@ whatis
     self
         что это?
     moth
-        находка с обескового крушения недалеко от новой зеландии
-        в записях говорится, почти всё расплавилось, что немного странно...
-        во всяком случае... главная из них это вот эта большая
-        пьедесталы обычно означают "важно", но этот непомеченный
-        алекс сделал базовый скан, сказал напоминает сетевой узел
-        мы даже не знаем наверняка, примет ли оно мыслекол
-        ну, тебе платят так или иначе, так что... терзай
+        находка с обесковских обломков под новой зеландией
+        в отчете указано, что большая часть расплавилась - странно это, честно говоря...
+        наибольшего внимания заслуживает элемент посередине
+        все что связано с пьедесталами обычно очень важно. странно, что этот экземпляр без метки.
+        алекс отсканировал внутреннюю структуру: говорит, мол, похоже на сетевой узел
+        не знаю даже, примет ли он мыслекол
+        тебе в любом случае заплатят, так что.. терзай.
         
     RESPONSES::self
         сесть<+>sit
@@ -177,15 +177,15 @@ whatis
         
 sit
     sourceless
-        металлический стул царапает бетон. ты садишься.
+        стальной стул громко скрежещет о бетон, когда ты садишься.
             EXEC::env.introSit()
             WAIT::3500
 
     sourceless
-        сиденье холодное и неудобное. свет от твоего защитного снаряжения рассеивается в перламутровых цистах.
+        стул холодный и неудобный. свет от твоей спецовки отражается в жемчужных глубинах цисты
         
     moth
-        как по старинке - мыслекол на готове
+        что делать сам знаешь. даю разрешение на активацию мыслекола
 
     RESPONSES::self
         что это<+>whatis
--- localization/their-vessel.js
+++ localization/their-vessel.js
@@ -11,7 +11,7 @@
 env.localization.page['localship'] = {dialogues: {},
     definitions: {}, 
     strings: {
-        "this is really that first contact ship, damn. did you know they turned it into a museum?": "ну ничего себе, эт реально тот самый корабль который установил первый контакт. ты знаешь что они из него музей сделали?",
+        "this is really that first contact ship, damn. did you know they turned it into a museum?": "ну ничего себе, эт реально тот самый корабль который установил первый контакт. ты знал что из него музей сделали?",
         "so the \"low cohesion\" here probably refers to that... other place, being connected": "так видимо \"низкая связность\" здесь подразумевает то... другое место",
         "that's weird, does this area seem bigger now?": "странно, тебе это место не кажется больше прежнего?",
         "figures we finally get a compatible corrucyst with some real data, and it has something destroying it": "ну разумеется, мы получаем совместимую корруцисту с реальными данными - и её непонятно что ломает изнутри"
